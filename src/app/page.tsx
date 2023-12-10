24efe4614888eb8e976c5246788b47238869381d
Timur <timurrahmatullaev703@gmail.com>
2023-12-10 18:37:46 +0600

    added interface SliderSlidesData to bind types for dataSlider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-
-import Image from 'next/image'
 import Header from '../app/components/Header'
 import Body from '../app/components/Body'
 import Footer from '../app/components/Footer'
